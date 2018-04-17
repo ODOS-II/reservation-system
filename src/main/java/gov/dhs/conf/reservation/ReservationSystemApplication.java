b3a6f68859239527de20a616923f9f518982bf23
David Semke <david.semke@slalom.com>
2018-04-17 12:47:07 -0500

    Add source code formatter and reformat

--- src/main/java/gov/dhs/conf/reservation/ReservationSystemApplication.java
+++ src/main/java/gov/dhs/conf/reservation/ReservationSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationSystemApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReservationSystemApplication.class, args);
+  }
 }
--- src/main/java/gov/dhs/conf/reservation/config/ReservationConfig.java
+++ src/main/java/gov/dhs/conf/reservation/config/ReservationConfig.java
@@ -3,5 +3,4 @@ package gov.dhs.conf.reservation.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ReservationConfig {
-}
+public class ReservationConfig {}
