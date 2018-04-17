ee34816182703e7956cec305f9eadd5fc962edc1
David Semke <david.semke@slalom.com>
2018-04-17 14:26:35 -0500

    Add User entity and UserRepository

---
+++ src/main/java/gov/dhs/conf/reservation/entities/User.java
@@ -0,0 +1,79 @@
+package gov.dhs.conf.reservation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private String username;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String email;
+
+  private String password;
+
+  private UserType type;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(final String username) {
+    this.username = username;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(final String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(final String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(final String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(final String password) {
+    this.password = password;
+  }
+
+  public UserType getType() {
+    return type;
+  }
+
+  public void setType(final UserType type) {
+    this.type = type;
+  }
+
+  public enum UserType {
+    ADMIN,
+    EMPLOYEE
+  }
+}
