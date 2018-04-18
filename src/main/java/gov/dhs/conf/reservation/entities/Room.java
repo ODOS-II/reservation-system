5382a627d843e6f7b95c47b54240c2a30771c976
David Semke <david.semke@slalom.com>
2018-04-18 09:06:02 -0500

    Add Room entity

---
+++ src/main/java/gov/dhs/conf/reservation/entities/Room.java
@@ -0,0 +1,53 @@
+package gov.dhs.conf.reservation.entities;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Room {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private int buildingId;
+  private String name;
+  private int occupancy;
+
+  @OneToMany private List<AVEquipment> avEquipment;
+
+  public int getBuildingId() {
+    return buildingId;
+  }
+
+  public void setBuildingId(final int buildingId) {
+    this.buildingId = buildingId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public int getOccupancy() {
+    return occupancy;
+  }
+
+  public void setOccupancy(final int occupancy) {
+    this.occupancy = occupancy;
+  }
+
+  public List<AVEquipment> getAvEquipment() {
+    return avEquipment;
+  }
+
+  public void setAvEquipment(final List<AVEquipment> avEquipment) {
+    this.avEquipment = avEquipment;
+  }
+}
