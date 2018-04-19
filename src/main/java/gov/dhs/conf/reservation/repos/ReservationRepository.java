e5feb55721bdc7d97a02d40bb79c44e6a49d31cf
David Semke <david.semke@slalom.com>
2018-04-19 13:35:37 -0500

    Remove docker stuff (for now) and unused repo method

--- src/main/java/gov/dhs/conf/reservation/repos/ReservationRepository.java
+++ src/main/java/gov/dhs/conf/reservation/repos/ReservationRepository.java
@@ -2,14 +2,11 @@ package gov.dhs.conf.reservation.repos;
 
 import gov.dhs.conf.reservation.entities.Reservation;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
-  List<Reservation> findAllByUserId(long userId);
-
   boolean existsByRoomIdAndStartTimeBetween(
       long roomId, LocalDateTime startTime, LocalDateTime endTime);
 
