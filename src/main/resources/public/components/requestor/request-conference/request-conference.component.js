7dad5a2814859123cc630c4bc3495416fc72c983
Seth Thomas <Seth.Andrew.Thomas@ibm.com>
2018-04-20 13:50:27 -0400

    alerts / doesn't post from 8:00 - 17:00 This isn't completely working Form validation is also preventing time varables from being set, triggering allerts at wrong times

--- src/main/resources/public/components/requestor/request-conference/request-conference.component.js
+++ src/main/resources/public/components/requestor/request-conference/request-conference.component.js
@@ -1,143 +1,164 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    // Register 'requestConference' component, along with its associated controller and template
-    angular.module('requestConference').component('requestConference', {
-        templateUrl: 'components/requestor/request-conference/request-conference.template.html',
-        controller: ['$window', 'reservationService', 'userService', 'AllowedMinBookingRequestTimeHours',RequestConferenceController],
-        controllerAs: 'requestConf',
-    });
+  // Register 'requestConference' component, along with its associated controller and template
+  angular.module('requestConference').component('requestConference', {
+    templateUrl: 'components/requestor/request-conference/request-conference.template.html',
+    controller: ['$window', 'reservationService', 'userService', 'AllowedMinBookingRequestTimeHours', RequestConferenceController],
+    controllerAs: 'requestConf',
+  });
 
-    // Controller - data binds to view-home template
-    function RequestConferenceController($window, reservationService, userService, AllowedMinBookingRequestTimeHours) {
-        var self = this;
-       
-        self.findUsers = function() {
-	        	userService.readAll(function(res, err) {
-	        	if(res) {
-	        		self.users = res;
-	        	}
-	        });
+  // Controller - data binds to view-home template
+  function RequestConferenceController($window, reservationService, userService, AllowedMinBookingRequestTimeHours) {
+    var self = this;
+
+    self.findUsers = function () {
+      userService.readAll(function (res, err) {
+        if (res) {
+          self.users = res;
         }
-        
-        self.findUsers();
+      });
+    }
 
-        self.findRooms = function() {
-            reservationService.getRooms(function (success) {
-                if(success) {
-                    var rooms = success.data._embedded.rooms;
-                    self.rooms = rooms;
-//                    var roomIds = _.map(rooms ,'id');
-//                    reservationService.getReservationsForRooms(roomIds).then( function(allReservations) {
-//                        self.rooms = self.filterOnlyAvailableRooms(rooms, allReservations);
-//                    });
+    self.findUsers();
 
-                }
-            });
+    self.findRooms = function () {
+      reservationService.getRooms(function (success) {
+        if (success) {
+          var rooms = success.data._embedded.rooms;
+          self.rooms = rooms;
+          //                    var roomIds = _.map(rooms ,'id');
+          //                    reservationService.getReservationsForRooms(roomIds).then( function(allReservations) {
+          //                        self.rooms = self.filterOnlyAvailableRooms(rooms, allReservations);
+          //                    });
+
         }
+      });
+    }
 
-        self.findRooms();
+    self.findRooms();
 
-        self.filterOnlyAvailableRooms = (rooms, allReservations) => {
-        return rooms.filter(function(room) {
-        	var index = room._links.self.href.lastIndexOf('/');
-        	var id = room._links.self.href.substring(index +1);
-            var reservationsMap = _.find(allReservations, {roomId: id});
-            console.log(reservationMap);
-            if (!reservationsMap || !reservationsMap.reservations) return true;
-            var found =  _.find(reservationsMap.reservations, function(reservation) {
-                var alreadyReserved =  self.alreadyReservedForCurrentSlot(reservation);
-                return alreadyReserved;
-            });
-            return !found;
-        })
+    self.filterOnlyAvailableRooms = (rooms, allReservations) => {
+      return rooms.filter(function (room) {
+        var index = room._links.self.href.lastIndexOf('/');
+        var id = room._links.self.href.substring(index + 1);
+        var reservationsMap = _.find(allReservations, { roomId: id });
+        console.log(reservationMap);
+        if (!reservationsMap || !reservationsMap.reservations) return true;
+        var found = _.find(reservationsMap.reservations, function (reservation) {
+          var alreadyReserved = self.alreadyReservedForCurrentSlot(reservation);
+          return alreadyReserved;
+        });
+        return !found;
+      })
     };
 
-    self.alreadyReservedForCurrentSlot = function(reservation) {
-        var starts = moment(self.date).set(self.startHour, 'h').set(self.startMin, 'minutes');
-        var ends = moment(self.date).set(self.endHour, 'h').set(self.endMin, 'minutes');
+    self.alreadyReservedForCurrentSlot = function (reservation) {
+      var starts = moment(self.date).set(self.startHour, 'h').set(self.startMin, 'minutes');
+      var ends = moment(self.date).set(self.endHour, 'h').set(self.endMin, 'minutes');
 
-        var resStarts = moment(reservation.startTime);
-        var resEnds = moment(reservation.endTime);
+      var resStarts = moment(reservation.startTime);
+      var resEnds = moment(reservation.endTime);
 
-        if (starts.isBetween(resStarts, resEnds)) {
-            return true;
-        }
-        if (ends.isBetween(resStarts, resEnds)) {
-            return true;
-        }
-        return false;
+      if (starts.isBetween(resStarts, resEnds)) {
+        return true;
+      }
+      if (ends.isBetween(resStarts, resEnds)) {
+        return true;
+      }
+      return false;
     };
 
     self.reloadRooms = () => {
       self.findRooms();
     }
 
-        self.submit = () => {
-            var starts = moment(self.date).set(self.startHour, 'h').set(self.startMin, 'minutes');
-            starts.set({hour: self.startHour, minute: self.startMin, second: 0, millisecond: 0});
-            var ends = moment(self.date).set(self.endHour, 'h').set(self.endMin, 'minutes');
-            ends.set({hour: self.endHour, minute: self.endMin, second:0, millisecond: 0});
-            // set post data
-            var postData = {
-                user: self.user,
-                startTime: starts,
-                endTime: ends,
-                room: self.room,
-                title: self.conferenceTitle,
-                roomConfiguration: "MEETING"
-            };
+    self.submit = () => {
+      var starts = moment(self.date).set(self.startHour, 'h').set(self.startMin, 'minutes');
+      starts.set({ hour: self.startHour, minute: self.startMin, second: 0, millisecond: 0 });
+      var ends = moment(self.date).set(self.endHour, 'h').set(self.endMin, 'minutes');
+      ends.set({ hour: self.endHour, minute: self.endMin, second: 0, millisecond: 0 });
 
-      // create reservation
-      reservationService.create(self.room, postData, function (success) {
-        // usually rebind to DOM here, but in this case, redirect back to home, which pulls calender data
-        console.log("reservation using room: " + self.room);
-        console.log(postData);
-        $window.location.href = '/#!/home';
-      });
+      console.log("start: " + self.startAmPm);
+      console.log("end: " + self.endAmPm);
 
+      var tmpstart = starts.hours();
+      if (self.startAmPm === "PM") {
+        tmpstart = starts.hours() + 12;
+      }
+      var tmpend = ends.hours();
+      if (self.endAmPm === "PM") {
+        tmpend = ends.hours() + 12;
+      }
+
+      console.log("military start time: " + tmpstart);
+      console.log("military end timem: " + tmpend);
+
+      //8am-5pm
+      if (!(tmpstart >= 8 && tmpend <= 17)) {
+        alert("Hours must be between 8am and 5pm");
+      } else {
+        // set post data
+        var postData = {
+          user: self.user,
+          startTime: starts,
+          endTime: ends,
+          room: self.room,
+          title: self.conferenceTitle,
+          roomConfiguration: "MEETING"
         };
 
-        self.getDateValidationMessage = (hour, min) => {
-            if (hour && hour.$invalid && min &&  min.$invalid) return 'Please enter valid hour and minutes.';
-            if (hour && hour.$invalid) return 'Please enter valid hour.';
-            if (min && min.$invalid) return 'Please enter valid minutes.';
-            return '';
-        };
+        // create reservation
+        reservationService.create(self.room, postData, function (success) {
+          // usually rebind to DOM here, but in this case, redirect back to home, which pulls calender data
+          console.log("reservation using room: " + self.room);
+          console.log(postData);
+          $window.location.href = '/#!/home';
+        });
+      }
+
+    };
+
+    self.getDateValidationMessage = (hour, min) => {
+      if (hour && hour.$invalid && min && min.$invalid) return 'Please enter valid hour and minutes.';
+      if (hour && hour.$invalid) return 'Please enter valid hour.';
+      if (min && min.$invalid) return 'Please enter valid minutes.';
+      return '';
+    };
 
 
-        self.confRequestWindowValidation = (startHr, startMin, startAmPm,  endHour, endMin, endAmPm) => {
-            var start = momentFromHrMin(startHr, startMin, startAmPm);
-            var end = momentFromHrMin(endHour, endMin, endAmPm);
-            var duration = moment.duration(end.diff(start));
-            var minutes = parseInt(duration.asMinutes());
-            if (minutes > AllowedMinBookingRequestTimeHours * 60 ) return 'Conference request time exceeds minimum allowed 3 hours';
-            return '';
-        };
+    self.confRequestWindowValidation = (startHr, startMin, startAmPm, endHour, endMin, endAmPm) => {
+      var start = momentFromHrMin(startHr, startMin, startAmPm);
+      var end = momentFromHrMin(endHour, endMin, endAmPm);
+      var duration = moment.duration(end.diff(start));
+      var minutes = parseInt(duration.asMinutes());
+      if (minutes > AllowedMinBookingRequestTimeHours * 60) return 'Conference request time exceeds minimum allowed 3 hours';
+      return '';
+    };
 
-        self.init = function () {
-            var starts = moment();
-            var ends  = moment();
-            self.startHour = Number.parseInt(starts.format('h'));
-            self.endHour = Number.parseInt(ends.format('h'));
-            self.endMin = self.startMin =  0;
-            self.startAmPm = starts.format('A');
-            self.endAmPm = ends.format('A');
-            self.date = new Date();
-        };
-        self.init();
+    self.init = function () {
+      var starts = moment();
+      var ends = moment();
+      self.startHour = Number.parseInt(starts.format('h'));
+      self.endHour = Number.parseInt(ends.format('h'));
+      self.endMin = self.startMin = 0;
+      self.startAmPm = starts.format('A');
+      self.endAmPm = ends.format('A');
+      self.date = new Date();
+    };
+    self.init();
+  }
+
+  function momentFromHrMin(hr, min, amPm) {
+    var aMoment = moment();
+    if (angular.isDefined(hr) && angular.isDefined(min)) {
+      // aMoment.set(amPm && amPm === 'PM'? 12+hr : hr, 'h');
+      // aMoment.set(min, 'minutes');
+      aMoment.hour(amPm && amPm === 'PM' ? 12 + hr : hr);
+      aMoment.minutes(min);
     }
 
-    function momentFromHrMin( hr, min, amPm) {
-        var aMoment = moment();
-        if (angular.isDefined(hr) && angular.isDefined(min)) {
-            // aMoment.set(amPm && amPm === 'PM'? 12+hr : hr, 'h');
-            // aMoment.set(min, 'minutes');
-            aMoment.hour(amPm && amPm === 'PM'? 12+hr : hr);
-            aMoment.minutes(min);
-        }
+    return aMoment;
 
-        return aMoment;
-
-    }
+  }
 })();
