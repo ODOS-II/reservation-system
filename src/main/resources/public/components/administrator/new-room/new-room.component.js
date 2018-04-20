db1453faf3d4d6bbdf792b4223d935ed76a6003d
justice <justice>
2018-04-19 21:47:18 -0400

    Pulls equipment from endpoint and send with submit when creating room

--- src/main/resources/public/components/administrator/new-room/new-room.component.js
+++ src/main/resources/public/components/administrator/new-room/new-room.component.js
@@ -6,15 +6,19 @@
   module('newRoom').
   component('newRoom', {
     templateUrl: 'components/administrator/new-room/new-room.template.html',
-    controller: ['roomService', '$scope', '$location', NewRoomController],
+    controller: ['roomService', 'equipmentService', '$scope', '$location', NewRoomController],
     controllerAs: 'newRoom'
   });
 
-  function NewRoomController(roomService, $scope, $location) {
+  function NewRoomController(roomService, equipmentService, $scope, $location) {
 
     $scope.room = {occupancy: "10"};
+    
+    $scope.equipment = ["TV", "Phone", "Computer"];
 
     $scope.create = function() {
+    	console.log("ROOM:");
+    	console.log($scope.room);
       roomService.create($scope.room, function(response, error) {
         if(response) {
           $location.path('/rooms/');
@@ -27,6 +31,20 @@
     $scope.cancel = function() {
       $location.path('/rooms/');
     }
+    
+    $scope.getAvList = function(){
+    	console.log("Getting av list");
+        equipmentService.readAll(function(res, error) {
+          if(res) {
+        	  console.log("results:::::", res);
+            $scope.equipment = res;
+          } else if(error) {
+            $scope.error = true;
+          }
+        });
+    }
+    
+    $scope.getAvList();
   }
 
 })();
